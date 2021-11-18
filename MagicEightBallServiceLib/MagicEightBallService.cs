c3df60a5c62ca36932cfdcbd9cd99bb11a089be3
Osazee Isere <oz1.concepts@gmail.com>
2021-11-18 05:31:54 +0100

    Initial commit

---
+++ MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService:IEightBall
+    {
+        //just for display purposes on the host
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awaits your question... ");
+        }
+      public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers = {"Future uncertain","Yes","No","Hazy","Ask again latter"
+            ,"Defiinitely"};
+
+            //return  a random response
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+
+        }
+
+    }
+}
